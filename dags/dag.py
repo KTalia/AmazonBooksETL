0b9a8d98cad85888f880b672c84d6744dddd330d
KTalia <kastrati.talia1@gmail.com>
2025-04-04 17:36:45 +0200

    Working dag

---
+++ dags/dag.py
@@ -0,0 +1,149 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_default_chrome_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+
+
+    return options
+
+def get_data_books(num_books,ti):
+    # SETUP
+    options = get_default_chrome_options()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+    time.sleep(5)
+
+    book_data = []
+    seen_titles = set()
+    page = 1
+
+    while len(book_data) < num_books:
+        url = f"https://www.literatura.mk/knigi/page-{page}" if page > 1 else "https://www.literatura.mk/knigi/"
+        driver.get(url)
+        time.sleep(3)
+        books = driver.find_elements(By.CSS_SELECTOR, ".item-data.col-xs-12.col-sm-12")
+
+        if not books:  
+            print("No books found on the page. Exiting loop.")
+            break
+
+        for book in books:
+            try:
+                # title
+                title_div = book.find_element(By.CLASS_NAME, "title")
+                title = title_div.find_element(By.TAG_NAME, "a").text.strip()
+
+                # author
+                try:
+                    author = book.find_element(By.CSS_SELECTOR, ".atributs-wrapper .value").text.strip()
+                except NoSuchElementException:
+                    author = "N/A"
+
+                # price
+                price = book.find_element(By.CLASS_NAME, "prices-wrapper .current-price")
+                clean_price = price.text.replace("МКД", "").strip()
+
+                # category
+                category = book.find_element(By.CLASS_NAME, "category-wrapper .category").text.strip()
+
+                if title not in seen_titles:
+                    seen_titles.add(title)
+                    book_data.append({
+                        "Title": title,
+                        "Author": author,
+                        "Price": clean_price,
+                        "Category": category,
+                    })
+
+                    if len(book_data) >= num_books:
+                        break
+
+            except Exception as e:
+                print(f"Error extracting book details: {e}")
+
+        page += 1
+
+    books_df = pd.DataFrame(book_data)
+    books_df.drop_duplicates(subset="Title", inplace=True)
+
+    # books_df.to_csv('books_data2.csv', index=False, encoding='utf-8-sig')
+    driver.quit()
+
+    ti.xcom_push(key='book_data', value=book_data)
+
+
+
+def insert_book_data_into_postgres(ti):
+    book_data = ti.xcom_pull(key='book_data', task_ids='fetch_book_data')
+    if not book_data:
+        raise ValueError("No book data found")
+
+    postgres_hook = PostgresHook(postgres_conn_id='books_connection')
+    insert_query = """
+    INSERT INTO books (title, author, price, category)
+    VALUES (%s, %s, %s, %s)
+    """
+    for book in book_data:
+        postgres_hook.run(insert_query, parameters=(book['Title'], book['Author'], book['Price'], book['Category']))
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 3, 22),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'fetch_and_store_books',
+    default_args=default_args,
+    description='A simple DAG to fetch book data and store it in Postgres',
+    schedule_interval='@daily',
+)
+
+fetch_book_data_task = PythonOperator(
+    task_id='fetch_book_data',
+    python_callable=get_data_books,
+    op_args=[3], 
+    dag=dag,
+)
+
+create_table_task = SQLExecuteQueryOperator(
+    task_id='create_table',
+    conn_id='books_connection',
+    sql="""
+    CREATE TABLE IF NOT EXISTS books (
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        author TEXT,
+        price TEXT,
+        category TEXT
+    );
+    """,
+    dag=dag,
+)
+
+insert_book_data_task = PythonOperator(
+    task_id='insert_book_data',
+    python_callable=insert_book_data_into_postgres,
+    dag=dag,
+)
+
+create_table_task >> fetch_book_data_task >> insert_book_data_task
